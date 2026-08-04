0168e1099db82f566a69c639c5e2b9bae1ccfd45
clearbolee <112168475+clearbolee@users.noreply.github.com>
2026-08-04 05:40:48 +0100

    Fix Telegram CHAT_ID

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ CORS(app)
 
 
 BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-CHAT_ID = -7900090337
+CHAT_ID = -1004401856043
 bot = telebot.TeleBot(BOT_TOKEN)
 
 USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Mobile/15E148 Safari/604.1"
